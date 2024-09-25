f8540bff0a904cce429e9fff53f8ec7f1e667312
raethlein <benjamin.raethlein@gmail.com>
2024-09-25 11:01:54 +0200

    Add delta paths and message logging

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -119,7 +119,11 @@ import { SessionEventDispatcher } from "@streamlit/app/src/SessionEventDispatche
 import { UserSettings } from "@streamlit/app/src/components/StreamlitDialog/UserSettings"
 import { DefaultStreamlitEndpoints } from "@streamlit/app/src/connection/DefaultStreamlitEndpoints"
 import { MetricsManager } from "@streamlit/app/src/MetricsManager"
-import { StyledApp } from "@streamlit/app/src/styled-components"
+import {
+  MsgBundle,
+  MsgLogger,
+  StyledApp,
+} from "@streamlit/app/src/styled-components"
 import withScreencast, {
   ScreenCastHOC,
 } from "@streamlit/app/src/hocs/withScreencast/withScreencast"
@@ -181,6 +185,8 @@ interface State {
   appConfig: AppConfig
   autoReruns: NodeJS.Timeout[]
   inputsDisabled: boolean
+  handledMessageBundles: ForwardMsg[][]
+  handledMessageBundleMetadata: MessageBundleMetadata[]
 }
 
 const ELEMENT_LIST_BUFFER_TIMEOUT_MS = 10
@@ -210,6 +216,12 @@ export const showDevelopmentOptions = (
   }
   return hostIsOwner || isLocalhost()
 }
+
+// all messages between the script started and finished session
+let messageBundle: ForwardMsg[] = []
+type MessageBundleMetadata = { receivedAt?: number }
+let messageBundleMetadata: MessageBundleMetadata = {}
+let scriptFinishedMsg = false
 
 export class App extends PureComponent<Props, State> {
   private readonly endpoints: StreamlitEndpoints
@@ -312,6 +324,8 @@ export class App extends PureComponent<Props, State> {
       appConfig: {},
       autoReruns: [],
       inputsDisabled: false,
+      handledMessageBundles: [],
+      handledMessageBundleMetadata: [],
     }
 
     this.connectionManager = null
@@ -668,6 +682,30 @@ export class App extends PureComponent<Props, State> {
    * Callback when we get a message from the server.
    */
   handleMessage = (msgProto: ForwardMsg): void => {
+    messageBundle.push(msgProto)
+
+    if (msgProto.type === "newSession") {
+      messageBundleMetadata.receivedAt = Date.now()
+    } else if (msgProto.type === "scriptFinished") {
+      scriptFinishedMsg = true
+    } else if (msgProto.type === "sessionStatusChanged" && scriptFinishedMsg) {
+      scriptFinishedMsg = false
+      this.setState((prevState: State) => {
+        return {
+          handledMessageBundles: [
+            messageBundle,
+            ...prevState.handledMessageBundles,
+          ],
+          handledMessageBundleMetadata: [
+            messageBundleMetadata,
+            ...prevState.handledMessageBundleMetadata,
+          ],
+        }
+      })
+      messageBundle = []
+      messageBundleMetadata = {}
+    }
+
     // We don't have an immutableProto here, so we can't use
     // the dispatchOneOf helper
     const dispatchProto = (obj: any, name: string, funcs: any): any => {
@@ -1997,6 +2035,34 @@ export class App extends PureComponent<Props, State> {
               {renderedDialog}
             </StyledApp>
           </Hotkeys>
+          <MsgLogger>
+            {this.state.handledMessageBundles.map((bundle, bundleIndex) => {
+              return (
+                <MsgBundle key={bundleIndex}>
+                  <div>
+                    Bundle (receivedAt:{" "}
+                    {
+                      this.state.handledMessageBundleMetadata[bundleIndex]
+                        .receivedAt
+                    }
+                    ):
+                  </div>
+                  {bundle.map((msg, index) => {
+                    const parsedMsg = {
+                      scriptRunId: msg.newSession?.scriptRunId,
+                      type: msg.type,
+                      fragmentId: msg.delta?.fragmentId,
+                    }
+                    return (
+                      <div key={msg.hash + index}>
+                        {JSON.stringify(parsedMsg)}
+                      </div>
+                    )
+                  })}
+                </MsgBundle>
+              )
+            })}
+          </MsgLogger>
         </LibContext.Provider>
       </AppContext.Provider>
     )
