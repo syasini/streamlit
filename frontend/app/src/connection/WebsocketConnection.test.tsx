ecb1c5016c95fbb34b97d17d8434d46f839a7e22
lukasmasuch <lukas.masuch@gmail.com>
2024-11-14 17:08:25 +0100

    Fix tests

--- frontend/app/src/connection/WebsocketConnection.test.tsx
+++ frontend/app/src/connection/WebsocketConnection.test.tsx
@@ -283,18 +283,6 @@ describe("doInitPings", () => {
       },
     }
 
-    const NoResponse = (
-      <Fragment>
-        <p>
-          Is Streamlit still running? If you accidentally stopped Streamlit,
-          just restart it in your terminal:
-        </p>
-        <StyledPre>
-          <StyledBashCode>streamlit run yourscript.py</StyledBashCode>
-        </StyledPre>
-      </Fragment>
-    )
-
     axios.get = vi
       .fn()
       // First Connection attempt
@@ -314,7 +302,7 @@ describe("doInitPings", () => {
 
     expect(MOCK_PING_DATA_LOCALHOST.retryCallback).toHaveBeenCalledWith(
       1,
-      NoResponse,
+      expect.anything(),
       expect.anything()
     )
   })
