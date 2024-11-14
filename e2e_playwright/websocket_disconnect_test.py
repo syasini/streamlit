4f6d882696c0e6241b069b743cb9b3c217cd4d24
lukasmasuch <lukas.masuch@gmail.com>
2024-11-14 20:09:45 +0100

    Add back websocket disconnect test

--- e2e_playwright/websocket_disconnect_test.py
+++ e2e_playwright/websocket_disconnect_test.py
@@ -12,57 +12,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from playwright.sync_api import Page, expect
 
-# def test_disconnected_states(app: Page, assert_snapshot: ImageCompareFunction):
-#     expect(app.get_by_test_id("stButton").locator("button")).not_to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stMarkdown").first).to_contain_text("Value 1: 25")
+from e2e_playwright.conftest import ImageCompareFunction
 
-#     expect(app.get_by_test_id("stConnectionStatus")).not_to_be_visible()
 
-#     # activating this will disable all elements and simulate runtime shutdown
-#     app.evaluate("window.streamlitDebug.shutdownRuntime()")
-#     expect(app.get_by_test_id("stConnectionStatus")).to_contain_text("Connecting")
+def test_disconnected_states(app: Page, assert_snapshot: ImageCompareFunction):
+    expect(app.get_by_test_id("stButton").locator("button")).not_to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stMarkdown").first).to_contain_text("Value 1: 25")
 
-#     expect(app.get_by_test_id("stButton").locator("button")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stCheckbox").locator("input")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stDateInput").locator("input")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stRadio").locator("input").first).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stSelectbox").locator("input")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stTextArea").locator("textarea")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stTextInput").locator("input")).to_have_attribute(
-#         "disabled", ""
-#     )
-#     expect(app.get_by_test_id("stTimeInput").locator("input")).to_have_attribute(
-#         "disabled", ""
-#     )
+    expect(app.get_by_test_id("stConnectionStatus")).not_to_be_visible()
 
-#     app.get_by_test_id("stSlider").nth(0).hover()
-#     # click in middle
-#     app.mouse.down()
+    # activating this will disable all elements and simulate runtime shutdown
+    app.evaluate("window.streamlitDebug.shutdownRuntime()")
+    expect(app.get_by_test_id("stConnectionStatus")).to_contain_text("Connecting")
 
-#     expect(app.get_by_test_id("stMarkdown").first).to_contain_text("Value 1: 25")
+    expect(app.get_by_test_id("stButton").locator("button")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stCheckbox").locator("input")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stDateInput").locator("input")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stRadio").locator("input").first).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stSelectbox").locator("input")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stTextArea").locator("textarea")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stTextInput").locator("input")).to_have_attribute(
+        "disabled", ""
+    )
+    expect(app.get_by_test_id("stTimeInput").locator("input")).to_have_attribute(
+        "disabled", ""
+    )
 
-#     # After some time the disconnected dialog will appear.
-#     # It would be nicer to have this in a separate function, but we can't do that easily
-#     # because the runtime is shutdown for all test functions. We would need to start the
-#     # runtime again somehow or move this to a separate file.
-#     dialog = app.get_by_role("dialog")
-#     # the dialog might need a moment to appear after shutting down the runtime
-#     expect(dialog).to_be_visible(timeout=20000)
-#     # make sure that the close-x button is not focused
-#     dialog.blur(timeout=0)
-#     assert_snapshot(dialog, name="websocket_connection-disconnected_dialog")
+    app.get_by_test_id("stSlider").nth(0).hover()
+    # click in middle
+    app.mouse.down()
+
+    expect(app.get_by_test_id("stMarkdown").first).to_contain_text("Value 1: 25")
+
+    # After some time the disconnected dialog will appear.
+    # It would be nicer to have this in a separate function, but we can't do that easily
+    # because the runtime is shutdown for all test functions. We would need to start the
+    # runtime again somehow or move this to a separate file.
+    dialog = app.get_by_role("dialog")
+    # the dialog might need a moment to appear after shutting down the runtime
+    expect(dialog).to_be_visible(timeout=20000)
+    # make sure that the close-x button is not focused
+    dialog.blur(timeout=0)
+    assert_snapshot(dialog, name="websocket_connection-disconnected_dialog")
