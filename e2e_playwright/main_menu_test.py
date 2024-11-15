d36d639da7f243d9b69855908f6d7b2850f66922
lukasmasuch <lukas.masuch@gmail.com>
2024-11-15 18:23:40 +0100

    Remove unblurs

--- e2e_playwright/main_menu_test.py
+++ e2e_playwright/main_menu_test.py
@@ -34,9 +34,6 @@ def test_renders_settings_dialog_properly(
     dialog = themed_app.get_by_test_id("stDialog")
     expect(dialog).to_be_visible()
 
-    # Make sure nothing is focused
-    dialog.blur(timeout=0)
-
     assert_snapshot(dialog.get_by_role("dialog"), name="settings_dialog")
 
 
@@ -50,9 +47,6 @@ def test_renders_screencast_dialog_properly(
     themed_app.get_by_text("Record a screencast").click()
     dialog = themed_app.get_by_test_id("stDialog")
     expect(dialog).to_be_visible()
-
-    # Make sure nothing is focused
-    dialog.blur(timeout=0)
 
     assert_snapshot(dialog.get_by_role("dialog"), name="record_screencast_dialog")
 
@@ -100,9 +94,6 @@ def test_renders_clear_cache_dialog_properly(
         "Are you sure you want to clear the app's function caches?"
     )
 
-    # Make sure nothing is focused
-    dialog.blur(timeout=0)
-
     assert_snapshot(dialog.get_by_role("dialog"), name="clear_cache_dialog")
 
 
@@ -115,8 +106,5 @@ def test_renders_active_theme_dialog_properly(
 
     dialog = themed_app.get_by_test_id("stDialog")
     expect(dialog).to_be_visible()
-
-    # Make sure nothing is focused
-    dialog.blur(timeout=0)
 
     assert_snapshot(dialog.get_by_role("dialog"), name="edit_active_theme_dialog")
